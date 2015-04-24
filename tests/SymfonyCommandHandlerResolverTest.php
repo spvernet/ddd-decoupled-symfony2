ba40a7907231cb72062f15cc00a3121f9504cc9f
Nil Portugués <nil.portugues@atrapalo.com>
2015-04-24 10:01:50 +0200

    moar

---
+++ tests/SymfonyCommandHandlerResolverTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace NilPortugues\Test;
+
+use NilPortugues\SymfonyCommandHandlerResolver;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+/**
+ * Class SymfonyCommandHandlerResolverTest
+ * @package NilPortugues\Test
+ */
+class SymfonyCommandHandlerResolverTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SymfonyCommandHandlerResolver
+     */
+    private $resolver;
+
+    /**
+     * @var array
+     */
+    private $handlers = [
+        'dummyCommand' => 'std_class'
+    ];
+
+    /**
+     *
+     */
+    public function setUp()
+    {
+        $this->resolver = new SymfonyCommandHandlerResolver($this->getServiceContainerMock());
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    private function getServiceContainerMock()
+    {
+        $serviceContainer = $this
+            ->getMockBuilder(ContainerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $serviceContainer
+            ->expects($this->any())
+            ->method('get')
+            ->willReturn(new \stdClass);
+
+        return $serviceContainer;
+    }
+
+    /**
+     *
+     */
+    public function testItShouldResolveCommandToCommandHandler()
+    {
+        $commandHandler = $this->resolver->get($this->handlers, 'dummyCommand');
+        $this->assertInstanceOf('stdClass', $commandHandler);
+    }
+}
